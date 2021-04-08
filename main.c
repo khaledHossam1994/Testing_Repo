9b27b21ea91e419be41de633a79506d459ae5c85
khaled hossam <khaledhossam.1994@gmail.com>
2021-04-08 21:30:03 +0200

    Updating main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-/* Version: 2.0 */
+/* Version: 3.0 */
 
 #include <stdio.h>
 
 void main(void)
 {
-	printf("Welcome to embedded system foundation course!");
+	printf("Welcome to embedded system foundation!");
 }
